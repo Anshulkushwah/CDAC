9d5a655e023aef281ecaed3460098e358a856563
Anshul kushwah <80462223+Anshulkushwah@users.noreply.github.com>
2025-03-04 07:30:57 +0530

    Create Q4.c

---
+++ C Programming /Assignment4/Q4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    // Input size of array
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    // Declare array and input elements
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Reverse the array
+    for (int i = 0; i < n / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+
+    // Print the reversed array
+    printf("Reversed array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
